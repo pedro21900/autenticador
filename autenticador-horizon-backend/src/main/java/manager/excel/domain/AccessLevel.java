67c2bc22a335ec6b1137359a46a6a32368157baa
Pedro Lenonn Oliveira da Silva <pedro.lenonn@tre-pa.jus.br>
2021-09-23 10:06:50 -0300

    feat: união front e back

---
+++ autenticador-horizon-backend/src/main/java/manager/excel/domain/AccessLevel.java
@@ -0,0 +1,25 @@
+package manager.excel.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class AccessLevel  {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String description;
+
+}
