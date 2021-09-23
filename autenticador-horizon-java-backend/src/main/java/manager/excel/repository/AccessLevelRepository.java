53f3ca3d166f2dbf261960993d6a650023f1970c
Pedro Lenonn Oliveira da Silva <pedro.lenonn@tre-pa.jus.br>
2021-09-23 10:12:23 -0300

    refactor: delete arquivos desnesseários

--- autenticador-horizon-java-backend/src/main/java/manager/excel/repository/AccessLevelRepository.java
+++
@@ -1,9 +0,0 @@
-package manager.excel.repository;
-
-import manager.excel.domain.AccessLevel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccessLevelRepository extends JpaRepository<AccessLevel,Long>{
-}
--- autenticador-horizon-java-backend/src/main/java/manager/excel/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package manager.excel.repository;
-
-import manager.excel.domain.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
