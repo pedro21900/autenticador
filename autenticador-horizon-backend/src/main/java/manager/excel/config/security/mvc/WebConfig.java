cbc182841a7ad9878aaeccb6c6096f693b875de4
Pedro Lenonn Oliveira da Silva <pedro.lenonn@tre-pa.jus.br>
2021-09-29 09:28:51 -0300

    fix: erro mvc swagger issue #8

--- autenticador-horizon-backend/src/main/java/manager/excel/config/security/mvc/WebConfig.java
+++ autenticador-horizon-backend/src/main/java/manager/excel/config/security/mvc/WebConfig.java
@@ -1,9 +1,11 @@
 package manager.excel.config.security.mvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -12,5 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");// <- assim permite de qualquer origem, troque "/**" pelo seu dominio por exemplo "http://meudominio.com"
+    }
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
