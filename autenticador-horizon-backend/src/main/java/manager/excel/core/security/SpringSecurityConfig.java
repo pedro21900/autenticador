37af7c11670ed3cdc2133c6df36f8d4ff89c34c2
Lenonn <pedro21900@hotmail.com>
2021-10-09 19:45:24 -0300

    feat: oauth2 issue #7

--- autenticador-horizon-backend/src/main/java/manager/excel/core/security/SpringSecurityConfig.java
+++ autenticador-horizon-backend/src/main/java/manager/excel/core/security/SpringSecurityConfig.java
@@ -1,17 +1,19 @@
 package manager.excel.core.security;
 
 //import manager.excel.service.UserSecurityService;
+
 import manager.excel.service.UserSecurityService;
-    import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-    import org.springframework.security.authentication.AuthenticationManager;
-    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-    import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -19,35 +21,40 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 // aplica automaticamente a classe ao global WebSecur
 
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
     private UserSecurityService userSecurityService;
 
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userSecurityService)
-                .passwordEncoder(getPasswordEncoder());
-
-    }
-
-
+    @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(getPasswordEncoder());
+        provider.setUserDetailsService(userSecurityService);
+        return provider;
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+              /*  .userDetailsService(userSecurityService)
+                .passwordEncoder(getPasswordEncoder());*/
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
-    @Override
+ /*   @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.cors();
+    }*/
 
-    }
+
 }
